d00039bec160040690ce9bef157a5696197e0b7e
Oscar Mike <mike.claure@gmail.com>
2025-05-22 14:21:58 +0200

    fix bug about default folder value

--- ai_tools/tools/local.py
+++ ai_tools/tools/local.py
@@ -3,6 +3,7 @@ from typing import Optional
 import logging
 from utils.logger import Logger
 from ai_tools.tools.tools_abstract import AIToolsAbstract
+from typing import Optional
 
 class LocalStorageTool(AIToolsAbstract):
 
@@ -63,7 +64,7 @@ class LocalStorageTool(AIToolsAbstract):
             },
         ]
 
-    def list_local_files(self, local_folder: str) -> dict:
+    def list_local_files(self, local_folder: Optional[str] = None) -> dict:
         """
         Lists the files in a specified local folder.
 
@@ -79,7 +80,12 @@ class LocalStorageTool(AIToolsAbstract):
         """
         if local_folder in [None, "", "{}"]:
             local_folder = self.default_local_folder
+
+        if not isinstance(local_folder, str):
+            local_folder = str(local_folder)
+
         self.logger.debug(f"Listing files in local folder: '{local_folder}'")
+
         try:
             files = os.listdir(local_folder)
             self.logger.debug(f"Found {len(files)} files in local folder.")
